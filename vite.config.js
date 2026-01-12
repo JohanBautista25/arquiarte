d442184f1709e40716c0a6c65af7ece6473de05e
johan_bautista <johanbautista1022@gmail.com>
2026-01-11 20:57:39 -0500

    config to hosting

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/dist/',
   plugins: [react()],
   resolve: {
     alias: {
